99a3d262e88e263527860b1eb9eecf52c1c540eb
G1U5 <isaiasgius@outlook.es>
2024-09-29 18:07:21 -0300

    agregamos maquetado en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
   return (
-    <h1>Hola mundo</h1>
-  )
+    <main className="container my-4">
+      <h1 className="text-center">Primer proyecto react</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+        cumque nesciunt eius, sunt neque illum ratione. Assumenda sed ipsa modi
+        adipisci aut minus laborum labore dignissimos. Non beatae tempore
+        mollitia.
+      </p>
+    </main>
+  );
 }
 
-export default App
+export default App;
