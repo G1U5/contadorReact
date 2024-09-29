e169d6681181cefb7bb80bd853d731032649df8d
G1U5 <isaiasgius@outlook.es>
2024-09-29 20:42:42 -0300

    agregamos el maquetado del componente teoria

--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,3 +1,5 @@
+import Texto from "./Texto";
+
 const Informacion = () => {
   // Aqui agrego la mayor parte de logica
 
@@ -8,12 +10,14 @@ const Informacion = () => {
       <p>
         React es una biblioteca de JavaScript desarrollada por Facebook para
         construir interfaces de usuario. Se destaca por su enfoque en la
-        creación de aplicaciones web de una sola <strong>página SPA</strong> y su capacidad para
-        manejar la interfaz de manera eficiente mediante el uso de un <span className="textoResaltado">DOM
-        virtual.</span> React permite a los desarrolladores construir interfaces
-        dinámicas y reactivas de forma modular, lo que facilita el desarrollo y
-        el mantenimiento de aplicaciones a gran escala.
+        creación de aplicaciones web de una sola <strong>página SPA</strong> y
+        su capacidad para manejar la interfaz de manera eficiente mediante el
+        uso de un <span className="textoResaltado">DOM virtual.</span> React
+        permite a los desarrolladores construir interfaces dinámicas y reactivas
+        de forma modular, lo que facilita el desarrollo y el mantenimiento de
+        aplicaciones a gran escala.
       </p>
+      <Texto></Texto>
     </section>
   );
 };
