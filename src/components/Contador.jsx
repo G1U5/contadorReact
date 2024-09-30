a5475c8d6dc4ef58f0404f59b92da664d4e9c4fa
G1U5 <isaiasgius@outlook.es>
2024-09-30 15:52:07 -0300

    agregamos la funcion para restar el valor del state numero

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -5,17 +5,18 @@ const Contador = () => {
     // Crear un estado
     const [numero, setNumero] = useState(1);
 
-    const sumar = ()=>{
-        numero++;
-        console.log(numero)
-    }
+    // const sumar = ()=>{
+    //     // numero++;
+    //     setNumero(numero + 1);
+    //     console.log(numero)
+    // }
 
     return (
         <section className="text-center my-5">
             <h2>Contador</h2>
             <h4>{numero}</h4>
-            <button className="btn btn-primary me-2" onClick={sumar}>+1</button>
-            <button className="btn btn-danger">-1</button>
+            <button className="btn btn-primary me-2" onClick={()=>setNumero(numero +1)}>+1</button>
+            <button className="btn btn-danger" onClick={()=>setNumero(numero -1)}>-1</button>
         </section>
     );
 };
